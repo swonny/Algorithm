fe05ec1bc982165726e2b965e5899947dbf32184
swonny <wonny921@gmail.com>
2022-08-03 20:18:14 +0900

    08.03 Datastructure_Array 완성

---
+++ test.py
@@ -0,0 +1,20 @@
+# i = 0
+# n = int(input('n을 입력하세요: '))
+# x = []
+
+# while True:
+#     num = int(input(f'x[{i}]에 들어갈 수를 입력하세요: '))
+#     x.append(x)
+#     i += 1
+#     if i == n:
+#         break
+
+x = ['a', 'b', 'c', 'd', 'e']
+
+# reversed(x)
+# print(x[::-1])
+# x.reverse()
+# print(x)
+
+y = list(reversed(x))
+print(y)
