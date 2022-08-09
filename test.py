2057db1f48ab1b54786925a5a2fff1a66ac713d4
swonny <wonny921@gmail.com>
2022-08-09 15:01:31 +0900

    22.08.09 큐

--- test.py
+++ test.py
@@ -1,25 +1,3 @@
-from sys import stdin
+c = [1,2,3,4,5]
 
-def isBal(string):
-    stk = []
-    for e in string:
-        if e == '(' or e == '[':
-            stk.append(e)
-        elif e == ')':
-            if len(stk) == 0 or stk.pop() == '[':
-                return 'no'
-        elif e == ']':
-            if len(stk) == 0 or stk.pop() == '(':
-                return 'no'
-    if len(stk): return 'no'
-    return 'yes'
-
-while True:
-    string = str(stdin.readline().rstrip())
-    if string == '.':
-        break
-    else:
-        print(isBal(string))
-
-                
-    
+print(c.count(0))
--- 백준/1차원배열/test.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-a = map(int, list(input().split(' ')))
-# print(min(a), max(a))
-print(a)
