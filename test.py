f5868b2f7b9a801d0e17c5373f82d3cc930a98ed
swonny <wonny921@gmail.com>
2022-08-09 10:28:38 +0900

    22.08.08 스택- 백준 풀이중

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
-t = int(input())
+from sys import stdin
 
-for _ in range(t):
-    tmp = list(map(int, input().split()))
-    N, score = tmp[0], tmp[1:]
-    avg = sum(score)/N
-    res = [n for n in score if n > avg]
-    print(format(len(res)/N*100, '.3f'), end='%\n')
+def isBal(string):
+    stk = []
+    for e in string:
+        if e == '(' or e == '[':
+            stk.append(e)
+        elif e == ')':
+            if len(stk) == 0 or stk.pop() == '[':
+                return 'no'
+        elif e == ']':
+            if len(stk) == 0 or stk.pop() == '(':
+                return 'no'
+    if len(stk): return 'no'
+    return 'yes'
+
+while True:
+    string = str(stdin.readline().rstrip())
+    if string == '.':
+        break
+    else:
+        print(isBal(string))
+
+                
+    
