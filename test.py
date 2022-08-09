c57c376ab0cc62492029f5fb21d3eb5999875fac
swonny <wonny921@gmail.com>
2022-08-09 21:26:41 +0900

    22.08.09 백준 스택-스택수열까지 완성

--- test.py
+++ test.py
@@ -1,3 +1,43 @@
-c = [1,2,3,4,5]
+from sys import stdin
 
-print(c.count(0))
+n = int(input())
+stk = []
+ptr = 0 # 수열 ptr
+top = 0 # 스택 데이터 개수
+result = []
+
+numarr = list([input() for _ in range(n)])
+for num in range(1, n+1):
+    stk.append(num) # push
+    top += 1
+    result.append('+')
+
+    while top > 0 and stk[top - 1] == int(numarr[ptr]):
+        stk.pop()
+        top -= 1
+        result.append('-')
+        ptr += 1
+
+if(top > 0):
+    stk.reverse()
+    stkStr = ''.join(map(str, stk))
+    numarrStr = ''.join(map(str, numarr[ptr:]))
+    if stkStr == numarrStr:
+        result.append(stkStr.split())
+    else:
+        print('NO')
+        result= []
+
+for n in result:
+    print(n)
+
+
+#     8
+# 4
+# 3
+# 6
+# 8
+# 7
+# 5
+# 2
+# 1
