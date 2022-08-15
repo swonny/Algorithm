d59a82e4d886c51eb30404f91182eefe24cc7bcd
swonny <wonny921@gmail.com>
2022-08-15 10:49:24 +0900

    22.08.15 단계별 -재귀 별찍기-10 풀이 필요

--- test.py
+++ test.py
@@ -1,25 +1,18 @@
-# 개선 코드
+n = int(input())
 
-from sys import stdin
+def recur(n, underbar):
+    if n < 1:
+        return
+    print('____'*underbar, end='')
+    print('''"재귀함수가 뭔가요?"
+"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.
+마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.
+그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."
+''')
+    recur(n-1, underbar + 1)
+    print('____'*underbar, end='')
+    print('''라고 답변하였지.''')
+    
 
-n = int(stdin.readline())
-arr = list(map(int, stdin.readline().split()))
-
-stk = [] # 크기 비교 스택
-res = [] # 결과 저장 스택 : 출력 시 뒤집어야 함
-
-idx = -1
-while -idx <= len(arr):
-    if not stk:
-        res.append(-1)
-    else:
-        if stk[-1] > arr[idx]:
-            res.append(stk[-1])
-        else:
-            stk.pop()
-            continue
-    stk.append(arr[idx])
-    idx -= 1
-
-res.reverse()        
-print(' '.join(map(str, res)))
+print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
+recur(n, 0)
