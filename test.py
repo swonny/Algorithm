bfaa60e190c1947c303939f2db258f2cd388b843
swonny <wonny921@gmail.com>
2022-08-23 19:55:23 +0900

    22.08.23 백준 반복문

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-from sys import stdin
-from collections import deque
-
-print(t)
+N = str(input())
+tmp = list(map(str, '0' + N))
+cnt = 0
+n = len(tmp)
+print(int(tmp[n - 2] + tmp[n - 1]))
+while cnt < 3:
+    cnt += 1
+    n = len(tmp)
+    print(int(tmp[n - 1]) + int(tmp[n - 2]))
--- test2.py
+++ test2.py
@@ -1,30 +1,12 @@
-# 이진 삽입 정렬 알고리즘 구현하기
-
-def binary_insertion_sort(a):
-    n = len(a)
-    for i in range(1, n):
-        key = a[i]
-        pl = 0
-        pr = i - 1
-    
-        while True:
-            pc = (pl + pr) // 2
-            if a[pc] == key:
-                break
-            elif a[pc] < key:
-                pl = pc + 1
-            else:
-                pr = pc - 1
-            if pl > pr:
-                break
-    
-    pd = pc + 1 if pl <= pr else pr + 1
-    
-    for j in range(i, pd, -1):
-        a[j] = a[j - 1]
-    a[pd] = key
-
-arr = [6, 4, 3, 7, 1, 9, 8]
-binary_insertion_sort(arr)
-
-print(arr)
+n = list(map(str, input()))
+N = int(''.join(n))
+tmp = -1
+cnt -= 1
+while tmp != N:
+    cnt += 1
+    tmp = sum(N)
+# print(a, b)
+# tmp = -1
+# cnt = 0
+# while tmp != N:
+#     a, b = str(N.split())
