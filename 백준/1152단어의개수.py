a90800cbd316fcf7618bc4bbf2c85ecc097f148f
swonny <wonny921@gmail.com>
2022-09-14 22:48:53 +0900

    22.09.14 백준 정렬

--- 백준/1152단어의개수.py
+++ 백준/1152단어의개수.py
@@ -1,17 +1,9 @@
-from collections import Counter
 from sys import stdin
 
-input = stdin.readline
 n = int(input())
-num = sorted([int(input()) for _ in range(n)])
+users = [stdin.readline().split()+[i] for i in range(n)]
+users = [(int(age), name, i) for i, (age, name) in enumerate(users)]
+users = sorted(users, key=lambda x: (x[0], x[2]))
 
-print(round(sum(num)/len(num))) # 평균
-print(num[n//2]) # 중앙값
-tmp = Counter(num)
-tmp = tmp.items()
-tmp = sorted(tmp, key=lambda x: (-x[1], x[0]))
-if n == 1 or tmp[0][1] > tmp[1][1]:
-    print(tmp[0][0])
-else:
-    print(tmp[1][0])
-print(max(num) - min(num)) # 범위
+for i in range(n):
+    print(f'{users[i][0]} {users[i][1]}')
