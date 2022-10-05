7e9dde27db1bf14b13986bb8c8672259d1757d14
swonny <wonny921@gmail.com>
2022-10-05 23:38:52 +0900

    22.10.05 백준-그리디

--- test2.py
+++ test2.py
@@ -1,14 +1,21 @@
 from sys import stdin
 
-t = int(input())
+n = int(input())
+meetings = [tuple(map(int, stdin.readline().strip().split())) for _ in range(n)]
+meetings = sorted(meetings)
 
-for _ in range(t):
-    h, w, n = map(int, stdin.readline().strip().split())
-    remainder = n % h
-    if remainder:
-        tmp = '0' + str((n // h) + 1)
-        print(tmp, remainder)
-        print(str(remainder) + tmp[-2:])
-    else:
-        tmp = '0' + str(h)
-        print(str(n // h) + tmp[-2:])
+s, e, t = meetings[0][0], meetings[0][1], meetings[0][1] - meetings[0][0]
+count = 1
+for meeting in meetings[1:]:
+    if meeting[1] - meeting[0] < t and meeting[1] <= e:
+        s = meeting[0]
+        e = meeting[1]
+        t = meeting[1] - meeting[0]
+    elif meeting[0] >= e:
+        s = meeting[0]
+        e = meeting[1]
+        t = meeting[1] - meeting[0]
+        count += 1
+    print(meeting, count)
+
+print(count)
